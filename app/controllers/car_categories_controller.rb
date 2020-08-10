f5bbfcd251c64f1e72ecf1c634659900c5ba1542
Murimaral <murimaral@gmail.com>
2020-08-10 17:12:48 -0300

    Sessao 04

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -5,5 +5,15 @@ class CarCategoriesController < ApplicationController
   def show
     @car_category = CarCategory.find(params[:id])
   end  
+  def new
+    @car_category = CarCategory.new
+  end  
+  def create 
+    puts '======================'
+    car_category_params = params.require(:car_category).permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
+    puts '======================'
+    @car_category = CarCategory.create(car_category_params)
+    redirect_to car_category_path(id: @car_category.id) #tambem poderia por (@car_category) ou redirect_to @car_category
+  end  
 end
     
