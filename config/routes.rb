b5aef0e7839ec0abb86ab357de7e2c30a20cd09f
Murimaral <murimaral@gmail.com>
2020-08-12 17:07:53 -0300

    ultimos 10min da sessao 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'  #portas de entrada da app
-  resources :car_categories, :subsidiaries
+  resources :car_categories, :subsidiaries, only: [:index, :new, :show, :create] 
 end
