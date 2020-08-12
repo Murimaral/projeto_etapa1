7bb1ea990097962774d5dcc436a4068d31659f28
Murimaral <murimaral@gmail.com>
2020-08-12 16:40:14 -0300

    aula sessao 5

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -12,8 +12,13 @@ class CarCategoriesController < ApplicationController
     puts '======================'
     car_category_params = params.require(:car_category).permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
     puts '======================'
-    @car_category = CarCategory.create(car_category_params)
-    redirect_to car_category_path(id: @car_category.id) #tambem poderia por (@car_category) ou redirect_to @car_category
+    @car_category = CarCategory.new(car_category_params)
+    if @car_category.save  #com os dados que veio do formulario, da pra salvar?
+      redirect_to car_category_path(id: @car_category.id) #tambem poderia por (@car_category) ou redirect_to @car_category
+    else
+      render :new #ele renderiza porem sem solicitar uma outra action
+      # ao utilizar o redirect_to new_car_category_path, é solicitado uma action do controller e nesse caso, resetaria a variavel @car_category e apagaria os erros do metodo anterior
+    end  
   end  
   # possivel criar um metodo PRIVATE chamado def car_category_params e no escopo teria params.require(:car_category).permit(:name, :daily_rate, :car_insurance, :third_party_insurance)
 end
--- app/models/car_category.rb
+++ app/models/car_category.rb
@@ -1,2 +1,4 @@
 class CarCategory < ApplicationRecord
+  validates :name, :daily_rate, :car_insurance, :third_party_insurance, presence: {message: 'não pode ficar em branco'}   
+  validates :name, uniqueness: {message: 'já está em uso', case_sensitive: false}
 end
